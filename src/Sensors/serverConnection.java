4d41d6ae817ea2fc2c9f2389cfea76daa73b3cfd
MrTolins <ferantoli22@gmail.com>
2021-07-27 15:02:12 +0200

    XML sobreescribe el mismo archivo y con mismo nombre

--- src/Sensors/serverConnection.java
+++ src/Sensors/serverConnection.java
@@ -64,8 +64,6 @@ public class serverConnection extends javax.swing.JFrame {
         txtDuration = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
         btnSave = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
-        txtNombre = new javax.swing.JTextField();
 
         jList1.setModel(new javax.swing.AbstractListModel<String>() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
@@ -123,37 +121,28 @@ public class serverConnection extends javax.swing.JFrame {
             }
         });
 
-        jLabel3.setText("Nombre del archivo:");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 432, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(104, 104, 104)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addComponent(jLabel2))
-                            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))
-                        .addGap(18, 18, 18)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(txtFrequency, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(txtDuration, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGap(13, 13, 13)
-                                .addComponent(btnSave)))
-                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, 432, Short.MAX_VALUE)
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
-                .addGap(114, 114, 114)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, 88, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(136, 136, 136)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jLabel2))
+                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtFrequency, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtDuration, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(13, 13, 13)
+                        .addComponent(btnSave)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
@@ -161,11 +150,7 @@ public class serverConnection extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(20, 20, 20)
                 .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 37, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel3)
-                    .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(19, 19, 19)
+                .addGap(37, 37, 37)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(txtFrequency, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -177,7 +162,7 @@ public class serverConnection extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jButton1)
                     .addComponent(btnSave))
-                .addContainerGap(84, Short.MAX_VALUE))
+                .addContainerGap(106, Short.MAX_VALUE))
         );
 
         pack();
@@ -215,123 +200,121 @@ public class serverConnection extends javax.swing.JFrame {
     }//GEN-LAST:event_txtFrequencyKeyTyped
 
     private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
-         try {
-      //Con estas instrucciones se obtiene en memoria el contenido completo del fichero XML
-      DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-      //Elemento raíz
-      Document doc = docBuilder.newDocument();
-      Element rootElement = doc.createElement("root");
-      doc.appendChild(rootElement);
-      //Primer elemento
-      Element elemento1 = doc.createElement("configuration");
-      rootElement.appendChild(elemento1);
-      //Hay métodos de la clase Document que permiten la creación de cada uno 
-      //de los posibles tipos de nodos admitidos por XML como CreateElement o CreateAttribute
-      //Se agrega un atributo al nodo frequency y su valor
-      Attr attr = doc.createAttribute("id");
-      attr.setValue("config");
-      elemento1.setAttributeNode(attr);
-      //Con frequency
-      Element frequency = doc.createElement("frequency");
-      frequency.setTextContent(txtFrequency.getText());
-      rootElement.appendChild(frequency);
-      //Con duration
-      Element duration = doc.createElement("duration");
-      duration.setTextContent(txtDuration.getText());
-      rootElement.appendChild(duration);
-      //Se escribe el contenido del XML en un archivo
-      //Para realizar cualquiera de esas operaciones, es necesario crear previamente un 
-      //transformador al que se le indique el documento y el destino que se le va a dar.
-      //La clase Transformer para generará un archivo de texto con el contenido del XML.
-      TransformerFactory transformerFactory = TransformerFactory.newInstance();
-      Transformer transformer = transformerFactory.newTransformer();
-      DOMSource source = new DOMSource(doc);
-      //Construcción de la ruta
-      
-      DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu-MM-dd");
-      LocalDate localDate = LocalDate.now();
-      
-      DateTimeFormatter dtt = DateTimeFormatter.ofPattern("HH-mm-ss");
-      LocalTime localTime = LocalTime.now();
-      
-      String nombreArchivo = txtNombre.getText();
-      String username = System.getProperty("user.name");
-      String Ruta = "C:\\Users\\" + username + "\\";
-      String XML = nombreArchivo + "-"+ dtf.format(localDate) + "-" + dtt.format(localTime) + ".xml";
-      String strRuta = Ruta + XML;
-      StreamResult result = new StreamResult(new File(strRuta));
- 
-      transformer.transform(source, result);
-      JOptionPane.showMessageDialog(null, "XML guardado en la ruta: " + strRuta, "Guardado", JOptionPane.INFORMATION_MESSAGE);
-    } catch (ParserConfigurationException pce) {
-      pce.printStackTrace();
-      
-    } catch (TransformerException tfe) {
-       
         
-     
-      DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu-MM-dd");
-      LocalDate localDate = LocalDate.now();
-      
-      DateTimeFormatter dtt = DateTimeFormatter.ofPattern("HH-mm-ss");
-      LocalTime localTime = LocalTime.now();
-      String nombreArchivo = txtNombre.getText();
-      String XML = nombreArchivo + "-"+ dtf.format(localDate) + "-" + dtt.format(localTime) + ".xml";
-      String username = System.getProperty("user.name");
-      String Ruta = "C:\\Users\\" + username;
-      String strRuta = Ruta + XML;
-      tfe.printStackTrace();
-      int warning = JOptionPane.showConfirmDialog(null, "Error al guardar el XML, no existe la ruta. ¿Desea crear la ruta " + strRuta + " ?", null, JOptionPane.YES_NO_OPTION);
-      if(warning == JOptionPane.YES_OPTION)
-      {
-          File directorio = new File(Ruta);
-          directorio.mkdirs();
-          File archivo = new File(Ruta,XML);
-          try {
-              //Mismo codigo que en btnSave Action Performance
-      DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-      
-      Document doc = docBuilder.newDocument();
-      Element rootElement = doc.createElement("root");
-      doc.appendChild(rootElement);
-      
-      Element elemento1 = doc.createElement("configuration");
-      rootElement.appendChild(elemento1);
-      
-      Attr attr = doc.createAttribute("id");
-      attr.setValue("config");
-      elemento1.setAttributeNode(attr);
-      
-      Element frequency = doc.createElement("frequency");
-      frequency.setTextContent(txtFrequency.getText());
-      rootElement.appendChild(frequency);
-      
-      Element duration = doc.createElement("duration");
-      duration.setTextContent(txtDuration.getText());
-      rootElement.appendChild(duration);
-      
-      TransformerFactory transformerFactory = TransformerFactory.newInstance();
-      Transformer transformer = transformerFactory.newTransformer();
-      DOMSource source = new DOMSource(doc);
-      StreamResult result = new StreamResult(new File(strRuta));
- 
-      transformer.transform(source, result);
-      JOptionPane.showMessageDialog(null, "XML guardado en la ruta: " + strRuta, "Guardado", JOptionPane.INFORMATION_MESSAGE);
-              
-          } catch (ParserConfigurationException ex) {
-              Logger.getLogger(serverConnection.class.getName()).log(Level.SEVERE, null, ex);
-          } catch (TransformerConfigurationException ex) {
-              Logger.getLogger(serverConnection.class.getName()).log(Level.SEVERE, null, ex);
-          } catch (TransformerException ex) {
-              Logger.getLogger(serverConnection.class.getName()).log(Level.SEVERE, null, ex);
-          }
-              
-      } 
-              
-    }
+        try {
+            //Con estas instrucciones se obtiene en memoria el contenido completo del fichero XML
+            DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+            //Elemento raíz
+            Document doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("root");
+            doc.appendChild(rootElement);
+            //Primer elemento
+            Element elemento1 = doc.createElement("configuration");
+            rootElement.appendChild(elemento1);
+            //Hay métodos de la clase Document que permiten la creación de cada uno 
+            //de los posibles tipos de nodos admitidos por XML como CreateElement o CreateAttribute
+            //Se agrega un atributo al nodo frequency y su valor
+            Attr attr = doc.createAttribute("id");
+            attr.setValue("config");
+            elemento1.setAttributeNode(attr);
+            //Con frequency
+            Element frequency = doc.createElement("frequency");
+            frequency.setTextContent(txtFrequency.getText());
+            rootElement.appendChild(frequency);
+            //Con duration
+            Element duration = doc.createElement("duration");
+            duration.setTextContent(txtDuration.getText());
+            rootElement.appendChild(duration);
+            //Se escribe el contenido del XML en un archivo
+            //Para realizar cualquiera de esas operaciones, es necesario crear previamente un 
+            //transformador al que se le indique el documento y el destino que se le va a dar.
+            //La clase Transformer para generará un archivo de texto con el contenido del XML.
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource source = new DOMSource(doc);
+            //Construcción de la ruta
 
+//            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+//            LocalDate localDate = LocalDate.now();
+//
+//            DateTimeFormatter dtt = DateTimeFormatter.ofPattern("HH-mm-ss");
+//            LocalTime localTime = LocalTime.now();
+//
+//            String nombreArchivo = txtNombre.getText();
+            String username = System.getProperty("user.name");
+            String Ruta = "C:\\Users\\" + username + "\\results\\";
+            String XML = "results.xml";
+            String strRuta = Ruta + XML;
+            StreamResult result = new StreamResult(new File(strRuta));
+
+            transformer.transform(source, result);
+            JOptionPane.showMessageDialog(null, "XML guardado en la ruta: " + strRuta, "Guardado", JOptionPane.INFORMATION_MESSAGE);
+        }catch(ParserConfigurationException pce) {
+            pce.printStackTrace();
+        }catch(TransformerException tfe) {
+       
+//            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+//            LocalDate localDate = LocalDate.now();
+//
+//            DateTimeFormatter dtt = DateTimeFormatter.ofPattern("HH-mm-ss");
+//            LocalTime localTime = LocalTime.now();
+//            String nombreArchivo = txtNombre.getText();
+            String XML = "results.xml";
+            String username = System.getProperty("user.name");
+            String Ruta = "C:\\Users\\" + username + "\\results\\";
+            String strRuta = Ruta + XML;
+            tfe.printStackTrace();
+            
+            int warning = JOptionPane.showConfirmDialog(null, "Error al guardar el XML, no existe la ruta. ¿Desea crear la ruta " + strRuta + " ?", null, JOptionPane.YES_NO_OPTION);
+      
+            if(warning == JOptionPane.YES_OPTION)
+            {
+                File directorio = new File(Ruta);
+                directorio.mkdirs();
+                File archivo = new File(Ruta,XML);
+
+                try {
+                    //Mismo codigo que en btnSave Action Performance
+                    DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+
+                    Document doc = docBuilder.newDocument();
+                    Element rootElement = doc.createElement("root");
+                    doc.appendChild(rootElement);
+
+                    Element elemento1 = doc.createElement("configuration");
+                    rootElement.appendChild(elemento1);
+
+                    Attr attr = doc.createAttribute("id");
+                    attr.setValue("config");
+                    elemento1.setAttributeNode(attr);
+
+                    Element frequency = doc.createElement("frequency");
+                    frequency.setTextContent(txtFrequency.getText());
+                    rootElement.appendChild(frequency);
+
+                    Element duration = doc.createElement("duration");
+                    duration.setTextContent(txtDuration.getText());
+                    rootElement.appendChild(duration);
+
+                    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+                    Transformer transformer = transformerFactory.newTransformer();
+                    DOMSource source = new DOMSource(doc);
+                    StreamResult result = new StreamResult(new File(strRuta));
+
+                    transformer.transform(source, result);
+                    JOptionPane.showMessageDialog(null, "XML guardado en la ruta: " + strRuta, "Guardado", JOptionPane.INFORMATION_MESSAGE);
+
+                } catch (ParserConfigurationException ex) {
+                    Logger.getLogger(serverConnection.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (TransformerConfigurationException ex) {
+                    Logger.getLogger(serverConnection.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (TransformerException ex) {
+                    Logger.getLogger(serverConnection.class.getName()).log(Level.SEVERE, null, ex);
+                }     
+            }  
+        }
     }//GEN-LAST:event_btnSaveActionPerformed
 
     /**
@@ -374,12 +357,10 @@ public class serverConnection extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JList<String> jList1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextField txtDuration;
     private javax.swing.JTextField txtFrequency;
-    private javax.swing.JTextField txtNombre;
     // End of variables declaration//GEN-END:variables
 }
